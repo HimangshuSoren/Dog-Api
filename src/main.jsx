00a7a36012c8ef0a4ec36fc829d9007535aafb07
Himangshu <67628306+HimangshuSoren@user.noreplay.github.com>
2025-03-18 11:53:36 +0530

    1st commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Dog from './Dog.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <Dog/>
 
-  </StrictMode>,
+  </>,
 )
